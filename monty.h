ccea1063a10a1acc9690e94fe50588ec413e441b
Noma-Afrika <142595257+NomaAfrika@users.noreply.github.com>
2023-12-15 12:11:43 +0200

    Update monty.h

--- monty.h
+++ monty.h
@@ -1,6 +1,6 @@
-#define _POSIX_C_SOURCE 200809L
 #ifndef MONTY_H
 #define MONTY_H
+#define_POSIX_C_SOURCE 200809L
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
@@ -77,4 +77,3 @@ void addqueue(stack_t **head, int n);
 void f_queue(stack_t **head, unsigned int counter);
 void f_stack(stack_t **head, unsigned int counter);
 #endif
-#define_POSIX_C_SOURCE 200809L
