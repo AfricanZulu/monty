2a487641967e47a638cb89c30e18e4ead2a44918
nomaAfrika <nomaAfrikagracezulu@gmail.com>
2023-12-17 06:26:33 -0800

    monty.h

--- monty.h
+++ monty.h
@@ -1,10 +1,5 @@
 #ifndef MONTY_H
 #define MONTY_H
-
-<<<<<<< HEAD
-=======
-#define_POSIX_C_SOURCE 200809L
->>>>>>> 772725bff20e46403348b7991a9b5bfb80a460a3
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
@@ -12,7 +7,6 @@
 #include <fcntl.h>
 #include <string.h>
 #include <ctype.h>
-
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
@@ -81,5 +75,4 @@ void addnode(stack_t **head, int n);
 void addqueue(stack_t **head, int n);
 void f_queue(stack_t **head, unsigned int counter);
 void f_stack(stack_t **head, unsigned int counter);
-
 #endif
